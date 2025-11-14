4c0b052714db125f9f2e4bc8ecbdebe3b9010183
cloud-digiteer <cloud@digiteer.digital>
2025-11-14 18:35:36 +0800

    Update os

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 # from dotenv import load_dotenv
-# import os
+import os
 
 
 # Load .env variables
